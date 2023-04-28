001493a22e83384f8cb56e00ca4ca4204f238da4
Daes <alex.bordyug.daes@gmail.com>
2023-04-28 19:46:52 +0300

    add KSR2_Practice

--- Test/KSR2_Practice/script.js
+++ Test/KSR2_Practice/script.js
@@ -1,4 +1,4 @@
-function InflatableBoat(boatLength, boatWeight, boatHeight, carrying, countOfSeats, country, isHasMotor){
+function InflatableBoat(boatLength, boatWeight, boatHeight, carrying, country, isHasMotor, countOfSeats){
     this.boatLength = boatLength;
     this.boatWeight = boatWeight;
     this.boatHeight = boatHeight;
@@ -16,16 +16,16 @@ InflatableBoat.prototype.getBoatCarrying = function(){
     return this.carrying < 220 ? this.carrying : `Данная грузоподъемность не соответствует лодке`
 }
 
-function Boat(boatLength, boatWeight, boatHeight, carrying, countOfSeats, country, isHasMotor, serviceLife){
-    InflatableBoat.call(this, boatLength, boatWeight, boatHeight, carrying, countOfSeats, country, isHasMotor)
+function Boat(boatLength, boatWeight, boatHeight, carrying, country, isHasMotor, countOfSeats, serviceLife){
+    InflatableBoat.call(this, boatLength, boatWeight, boatHeight, carrying, country, isHasMotor, countOfSeats)
     this.serviceLife = serviceLife;
 }
 
 Boat.prototype = Object.create(InflatableBoat.prototype);
 Boat.prototype.constructor = Boat;
 
-function Yacht(boatLength, boatWeight, boatHeight, carrying, countOfSeats, country, isHasMotor){
-    InflatableBoat.call(this, boatLength, boatWeight, boatHeight, carrying, countOfSeats, country, isHasMotor)
+function Yacht(boatLength, boatWeight, boatHeight, carrying, country, isHasMotor){
+    InflatableBoat.call(this, boatLength, boatWeight, boatHeight, carrying, country, isHasMotor)
 }
 
 Yacht.prototype = Object.create(InflatableBoat.prototype);
@@ -35,11 +35,11 @@ let inflatableBoat1 = new InflatableBoat(50, 50, 10, 10, 2, 'Беларусь', 
 let boat1 = new Boat(120, 200, 50, 190, 4, 'Беларусь', true, '10 лет')
 let yacht1 = new Yacht(300, 300, 200, 300, 20, 'Беларусь', true)
 
-console.log(inflatableBoat1.getBoatInfo());
-console.log(boat1.getBoatInfo());
-console.log(yacht1.getBoatInfo());
+console.log(inflatableBoat1.getBoatInfo()); // Длинна: 50, Ширина: 50, Вес: 10
+console.log(boat1.getBoatInfo()); // Длинна: 120, Ширина: 200, Вес: 50
+console.log(yacht1.getBoatInfo()); // Длинна: 300, Ширина: 300, Вес: 200
 
-console.log(inflatableBoat1.getBoatCarrying());
-console.log(boat1.getBoatCarrying());
-console.log(yacht1.getBoatCarrying());
+console.log(inflatableBoat1.getBoatCarrying()); // 10
+console.log(boat1.getBoatCarrying()); // 190
+console.log(yacht1.getBoatCarrying()); // Данная грузоподъемность не соответствует лодке
 
